95bf19052219b31d9fc685004cfceeab96372e8a
fardragon <michaldrozd@protonmail.ch>
2023-04-28 21:50:51 +0200

    Cleanup startup

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const mbox = @import("mailbox.zig").Mailbox;
 const serial = @import("serial.zig").Serial;
+const arm = @import("arm.zig");
 
 export fn remoter_main() noreturn {
+    arm.zeroBSS();
     serial.init();
     serial.println("Hello world!", .{});
     serial.println("Serial number: {}\r\n", .{mbox.get_serial_number() catch unreachable});
